a5f60691e8bd9d667b408a37b1cb660f78c0b2c0
Mihail <dsdmsa@gmail.com>
2019-02-17 17:50:12 -0800

    vc_isdigit.h #6

--- libvc/vc_isdigit.h
+++ libvc/vc_isdigit.h
@@ -6,9 +6,15 @@
 #ifndef UNTITLED1_VC_ISDIGIT_H
 #define UNTITLED1_VC_ISDIGIT_H
 
+#define TRUE  1
+#define FALSE  0
+
+
 int vc_isdigit(char c)
 {
-    return c > '0' && c < '9';
+    if (c >= '0' && c <= '9')
+        return TRUE;
+    return FALSE;
 }
 
 #endif //UNTITLED1_VC_ISDIGIT_H
--- main.c
+++ main.c
@@ -8,48 +8,76 @@
 
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
+#define TRUE  1
 
+static void print_init(char *fun_name)
+{
+    printf("%s ============ testing %s ============\n\n", KGRN,fun_name);
+}
 
-static int test_result(int res, char *func_name)
+static void print_end()
+{
+    printf("\n ================================================\n\n");
+}
+
+static void test_result(int res)
 {
     if (res)
     {
-        printf("%s test is passes for %s\n", KGRN, func_name);
+        printf("%s test is passes\n", KGRN);
     } else
     {
-        printf("%s test is NOT passes for %s\n", KRED, func_name);
+        printf("%s test is NOT passes\n", KRED);
 
     }
 }
 
-int test_vc_atoi()
+
+static void test_vc_atoi()
 {
+    print_init("test_vc_atoi\0");
     int n = 123;
     int test_n = vc_atoi("123\0");
-    test_result(n == test_n, "test_vc_atoi\0");
+    test_result(n == test_n);
 
     int n_2 = 23;
     int test_n_2 = vc_atoi("123\0");
-    test_result(n_2 != test_n_2, "test_vc_atoi\0");
+    test_result(n_2 != test_n_2);
 
     int n_3 = -23;
     int test_n_3 = vc_atoi("-23\0");
-    test_result(n_3 == test_n_3, "test_vc_atoi\0");
+    test_result(n_3 == test_n_3);
 
     int n_4 = -123;
     int test_n_4 = vc_atoi("-23\0");
-    test_result(n_4 != test_n_4, "test_vc_atoi\0");
+    test_result(n_4 != test_n_4);
 
     int n_5 = -990909;
     int test_n_5 = vc_atoi("-990909\0");
-    test_result(n_5 == test_n_5, "test_vc_atoi\0");
+    test_result(n_5 == test_n_5);
 
-    test_result(-103 == vc_atoi("-103\0"), "test_vc_atoi\0");
+    test_result(-103 == vc_atoi("-103\0"));
+
+    print_end();
+};
+
+void test_vc_isdigit()
+{
+    print_init("test_vc_isdigit\0");
+    test_result(vc_isdigit('0') == TRUE);
+    test_result(vc_isdigit('1') == TRUE);
+    test_result(vc_isdigit('2') == TRUE);
+    test_result(vc_isdigit('7') == TRUE);
+    test_result(vc_isdigit('8') == TRUE);
+    test_result(vc_isdigit('9') == TRUE);
+    print_end();
 }
+
 
 int main()
 {
     test_vc_atoi();
+    test_vc_isdigit();
 
     return 0;
 }
