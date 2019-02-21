bcdf732f8ba2678518c6a1eba0e64a96beae6058
Mihail <dsdmsa@gmail.com>
2019-02-20 21:11:35 -0800

    vc_memdel

--- main.c
+++ main.c
@@ -32,6 +32,7 @@
 #include "libvc/vc_strrchr.h"
 #include "libvc/vc_isprint.h"
 #include "libvc/vc_memset.h"
+#include "libvc/vc_memdel.h"
 
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
@@ -236,11 +237,11 @@ void test_vc_strsub()
 //    test_result(vc_strcmp(vc_strsub("I like pasta", "like", "love"), "I love pasta") == TRUE);
 
 //     print_init((char*)__func__);
-     char *source = "I like pasta";
+    char *source = "I like pasta";
 
-     test_result(vc_strcmp(vc_strsub(source, 0, 2), "I ") == TRUE);
-     test_result(vc_strcmp(vc_strsub(source, 6, 6), " pasta") == TRUE);
-     test_result((int)vc_strsub(source, 99999, 0) != TRUE);
+    test_result(vc_strcmp(vc_strsub(source, 0, 2), "I ") == TRUE);
+    test_result(vc_strcmp(vc_strsub(source, 6, 6), " pasta") == TRUE);
+    test_result((int) vc_strsub(source, 99999, 0) != TRUE);
 
 //     test_result(vc_strcmp(vc_strsub("asd", "asd", "dsa"), "dsa") == TRUE);
 //     test_result(vc_strcmp(vc_strsub("I like pasta", "pasta", "meat"), "I like meat") == TRUE);
@@ -369,7 +370,8 @@ void test_vc_striter()
     print_end();
 }
 
-void test_vc_memcpy() {
+void test_vc_memcpy()
+{
     print_init("test_vc_memcpy\0");
     char dest[100];
     char *src = "Pink Panther";
@@ -454,7 +456,8 @@ void test_vc_strdup()
     print_end();
 }
 
-void test_vc_memcmp() {
+void test_vc_memcmp()
+{
     print_init("test_vc_memcmp\0");
     char *str1 = "Dog Cat Tiger";
     char *str2 = "Dog Ant Rabbit";
@@ -469,8 +472,9 @@ void test_vc_memcmp() {
 
 }
 
-void test_vc_memset(){
-    print_init((char*)__func__);
+void test_vc_memset()
+{
+    print_init((char *) __func__);
     char str[50];
 
     vc_strcpy(str, "This is string library function");
@@ -576,7 +580,15 @@ void test_vc_putnbr()
     putchar('\n');
     vc_putnbr(1412341234);
     print_end();
+}
 
+void test_vc_memdel()
+{
+    print_init((char *) __func__);
+    char **str = (char**)malloc(2 * sizeof(char*));
+    vc_memdel(str);
+    test_result(*str == NULL);
+    print_end();
 }
 
 int main()
@@ -611,11 +623,12 @@ int main()
     test_vc_strnew();
     test_vc_puts();
     test_vc_strmap();
-    test_vc_strdup(); 
+    test_vc_strdup();
     test_vc_strrchr();
     test_vc_strdup();
     test_vc_isprint();
     test_vc_strsub();
     test_vc_memset();
+    test_vc_memdel();
     return 0;
 }
